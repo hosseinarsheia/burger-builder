e1de112809f200d4896ff614b5d0662bad1c843a
HO3EIN <hosseinarsheia@yahoo.com>
2018-12-14 12:34:49 +0330

    nothing special
    nothing special

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import  React , {Component} from 'react'
+import Aux from '../../hoc/myAux'
+
+
+class BurgerBuilder extends Component{
+    render(){
+        return(
+            <Aux>
+            <div>burger</div>
+            <div>burger controls</div>
+            </Aux>
+
+        )
+    }
+}
+
+export default BurgerBuilder;
